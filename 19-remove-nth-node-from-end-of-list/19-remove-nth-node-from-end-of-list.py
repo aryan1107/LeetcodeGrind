fb83b46630f1d3345f7d4d1c73a639115cbe465b
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2022-11-06 16:51:19 -0500

    Time: 75 ms (12.17%), Space: 13.9 MB (71.19%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,39 +5,14 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = fast =head
+        for _ in range(n):
+            fast = fast.next
+        if fast is None:
+            return head.next
+        while fast.next != None:
+            fast = fast.next
+            slow = slow.next
         
-        #reverse
-        curr = head
-        prev = None
-        while curr:
-            temp_next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp_next
-        if n == 1:
-            prev = prev.next
-            new_curr = prev
-            new_prev = None
-            while new_curr:
-                temp_next = new_curr.next
-                new_curr.next = new_prev
-                new_prev = new_curr
-                new_curr = temp_next
-            return new_prev
-        i = 1
-        curr = prev
-        while curr.next:
-            if i+1 == n:
-                curr.next = curr.next.next
-            else:
-                curr = curr.next
-            i+=1
-        
-        new_curr = prev
-        new_prev = None
-        while new_curr:
-            temp_next = new_curr.next
-            new_curr.next = new_prev
-            new_prev = new_curr
-            new_curr = temp_next
-        return new_prev
+        slow.next = slow.next.next
+        return head
